0ca57b577b3e25d830e40940747bd39ecafd4951
Benjamin Ladick <bladick@Benjamins-MacBook-Pro.local>
2023-02-18 02:13:34 -0700

    game solving

--- game_solver/gamecreator.py
+++ game_solver/gamecreator.py
@@ -3,11 +3,12 @@ from itertools import permutations
 from json import dumps
 from sys import argv
 
+board_size = 1
 card_values = [ str(i) for i in range(2, 10) ] + [ "T", "J", "Q", "K", "A" ]
 num_players = 2
 pot_size = 2
 constant_sum = 2
-game_cards = [ "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS" ]
+game_cards = [ "J", "Q", "K" ]
 
 kuhn_template = {
     "player": 0,
@@ -95,39 +96,52 @@ raise_kuhn_template = {
     ]
 }
 
-subgame_template = raise_kuhn_template
+subgame_template = ( kuhn_template )
 
-def valuate_card(card):
-    return { card_values[i]: i for i in range(len(card_values)) }[card[0]]
+def valuate_card(card, board=None):
+    hand_type = 0
 
-def showdown(card1, card2, profit):
+    if board is None:
+        board = set()
+    
+    board_values = set([ b[0] for b in board ])
+    
+    if card[0] in board_values:
+        hand_type = 1
+
+    return (hand_type << 4) | { card_values[i]: i for i in range(len(card_values)) }[card[0]]
+
+def showdown(card1, card2, profit, board=None):
     split = int(constant_sum / 2)
     loss = constant_sum - profit
-    value1 = valuate_card(card1)
-    value2 = valuate_card(card2)
+    value1 = valuate_card(card1, board)
+    value2 = valuate_card(card2, board)
     
     return profit if value1 > value2 \
         else split if value1 == value2 \
         else loss
 
 def is_util(node):
-    return type(node) is set
+    return type(node) is set or ( type(node) is dict and len(node.keys()) == 1 and list(node.keys())[0] == "utility" )
 
 def is_payoff(node):
     return type(node) is list
 
-def get_payoff(util_node, cards):
+def get_payoff(util_node, cards, board=None):
     profit = list(util_node)[0]
-    value1 = showdown(*cards, profit)
-    value2 = showdown(*cards[::-1], profit)
+    value1 = showdown(*cards, profit, board)
+    value2 = showdown(*cards[::-1], profit, board)
     
     return [ value1, value2 ]
     
-def gen_subgame(card_pair, template):
+def gen_subgame(card_pair, template, board=None):
+
+    if type(template) is int:
+        return template
     
     if is_util(template):
         return {
-            "utility": get_payoff(template, card_pair)
+            "utility": get_payoff(template, card_pair, board)
         }
     
     if is_payoff(template):
@@ -136,15 +150,66 @@ def gen_subgame(card_pair, template):
         }
     
     player = template["player"]
-    
-    template["info_state"] = template["info_state"] % card_pair[player]
+
+    if template["info_state"].count("%s") == 1:
+        template["info_state"] = template["info_state"] % card_pair[player]
+    else:
+        template["info_state"] = template["info_state"] % (card_pair[player], board)
     
     for i in range(len(template["next"])):
-        template["next"][i] = gen_subgame(card_pair, template["next"][i])
+        template["next"][i] = gen_subgame(card_pair, template["next"][i], board)
 
     return template
 
-def generate_game(template, cards=None):
+def add_action_history(info_state, board, action_history):
+    no_history = " -" not in info_state
+    player_card_str = f"{info_state} -" if no_history else info_state[:info_state.index(" -") + 2]
+    info_state_suffix = "" if no_history else f"- {info_state[(len(player_card_str) + 1):]}"
+    history_str = ' '.join(action_history)
+
+    return f"{player_card_str} {history_str} - {board} {info_state_suffix}".strip()
+
+def adjust_payoffs_and_actions(template, pot, board, action_history):
+
+    if is_util(template):
+
+        if template["utility"][0] != template["utility"][1]:
+            loss = int( (pot - constant_sum) / num_players )
+            winner_idx = 0 if template["utility"][0] > template["utility"][1] else 1
+            loser_idx = (winner_idx + 1) % 2
+
+            template["utility"][loser_idx] -= loss
+            template["utility"][winner_idx] += loss
+    
+    else:
+        template["info_state"] = add_action_history(template["info_state"], board, action_history)
+        for i in range(len(template["next"])):
+            template["next"][i] = adjust_payoffs_and_actions(template["next"][i], pot, board, action_history)
+
+    return template
+
+def append_postflop_subgame(preflop, postflop, board, action_history=None):
+
+    if action_history is None:
+        action_history = list()
+
+    if type(preflop) is int:
+        postflop_subgame = adjust_payoffs_and_actions(deepcopy(postflop), preflop, board, action_history)
+        return postflop_subgame
+    
+    if is_util(preflop):
+        return preflop
+    
+    else:
+        for i in range(len(preflop["next"])):
+            action = preflop["action_names"][i]
+            action_history.append(action)
+            preflop["next"][i] = append_postflop_subgame(preflop["next"][i], postflop, board, action_history)
+            action_history.pop()
+    
+    return preflop
+
+def generate_game(*templates, cards=None):
     global game_cards
 
     if cards is None:
@@ -152,19 +217,42 @@ def generate_game(template, cards=None):
 
     subgame_list = list()
     deals = permutations(cards, num_players)
+
+    if len(templates) == 1:
+        template = templates[0]
     
-    for deal in deals:
-        subgame_template = deepcopy(template)
-        subgame = gen_subgame(deal, subgame_template)
-        subgame_list.append(subgame)
+        for deal in deals:
+            subgame_template = deepcopy(template)
+            subgame = gen_subgame(deal, subgame_template)
+            subgame_list.append(subgame)
+
+    elif len(templates) == 2:
+        preflop = templates[0]
+        postflop = templates[1]
+
+        for deal in deals:
+
+            preflop_subgame = gen_subgame(deal, deepcopy(preflop))
+            deck = deepcopy(cards)
+            
+            for deal_card in deal:
+                deck.remove(deal_card)
+            
+            boards = permutations(deck, board_size)
+
+            for board in boards:
+                board_card = board[0]
+                postflop_subgame = gen_subgame(deal, deepcopy(postflop), board_card)
+                subgame = append_postflop_subgame(deepcopy(preflop_subgame), postflop_subgame, board_card)
+                subgame_list.append(subgame)
         
     game = { "next": subgame_list }
-    
+
     return game
 
 def main():
     filename = argv[1]
-    game = generate_game(subgame_template)
+    game = generate_game(*subgame_template)
     gamestr = dumps(game, indent=4)
     
     with open(filename, "w+") as f:
--- game_solver/test_gamecreator.py
+++ game_solver/test_gamecreator.py
@@ -15,6 +15,19 @@ class TestHandEvaluation(TestCase):
         self.assertEqual(8, value3)
         self.assertEqual(value3, value4)
     
+    def test_card_valuation_board_pair(self):
+        board = "JC"
+        value1 = "JH"
+        value2 = "AH"
+
+        evaluation = GC.valuate_card(value1, board)
+
+        self.assertEqual(16 + 9, evaluation)
+
+        evaluation = GC.valuate_card(value2, board)
+
+        self.assertEqual(12, evaluation)
+    
     def test_showdown_pays_off_correctly_on_winner(self):
         low = "J"
         high = "K"
@@ -52,6 +65,10 @@ class TestCardTemplating(TestCase):
         
         self.assertFalse(GC.is_util(payoff_node))
         self.assertFalse(GC.is_util(decision_node))
+    
+    def test_real_utility_node_is_utility_node(self):
+        utlity_node = { "utility": [ 2, 0 ] }
+        self.assertTrue(GC.is_util(utlity_node))
     
     def test_check_is_payoff(self):
         util_node = { 10 }
@@ -93,7 +110,7 @@ class TestCardTemplating(TestCase):
         
         self.assertEqual(expected_subgame, actual_subgame)
     
-    def test_subgame_template_generation(self):        
+    def test_basic_subgame_template_generation(self):        
         card_pair = ( "J", "Q" )
         subgame_template = {
             "player": 0,
@@ -132,7 +149,9 @@ class TestCardTemplating(TestCase):
         }
         
         self.assertEqual(expected_subgame, actual_subgame)
-        
+    
+    def test_subgame_template_generation_handing_payoffs_and_set_utilities(self):
+        card_pair = ( "J", "Q" )
         subgame_template = {
             "player": 0,
             "info_state": "%s",
@@ -204,8 +223,300 @@ class TestCardTemplating(TestCase):
         }
         
         self.assertEqual(expected_subgame, actual_subgame)
+    
+    def test_subgame_template_generation_integer_nodes_are_ignored(self):
+        card_pair = ( "J", "Q" )
+
+        subgame_template = {
+            "player": 0,
+            "info_state": "%s",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                [ 0, 2 ],
+                {
+                    "player": 1,
+                    "info_state": "%s - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        [ 2, 0 ],
+                        2
+                    ]
+                }
+            ]
+        }
+
+        actual_subgame = GC.gen_subgame(card_pair, subgame_template)
+
+        expected_subgame = {
+            "player": 0,
+            "info_state": "J",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                { "utility": [ 0, 2 ] },
+                {
+                    "player": 1,
+                    "info_state": "Q - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        2
+                    ]
+                }
+            ]
+        }
+
+        self.assertEqual(expected_subgame, actual_subgame)
+
+    def test_subgame_template_generation_with_board(self):
+        card_pair = ( "J", "Q" )
+        board = "J"
+
+        subgame_template = {
+            "player": 0,
+            "info_state": "%s - Check Check - %s",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                [ 0, 2 ],
+                {
+                    "player": 1,
+                    "info_state": "%s - Check Check - %s - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        [ 2, 0 ],
+                        { 3 }
+                    ]
+                }
+            ]
+        }
+
+        actual_subgame = GC.gen_subgame(card_pair, subgame_template, board)
+
+        expected_subgame = {
+            "player": 0,
+            "info_state": "J - Check Check - J",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                { "utility": [ 0, 2 ] },
+                {
+                    "player": 1,
+                    "info_state": "Q - Check Check - J - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        { "utility": [ 3, -1 ] }
+                    ]
+                }
+            ]
+        }
+
+        self.assertEqual(expected_subgame, actual_subgame)
+    
+    def test_add_board_and_history_to_info_state(self):
+        info_state = "J"
+        board = "J"
+        action_history = [ "Check", "Check" ]
+
+        new_state = GC.add_action_history(info_state, board, action_history)
+
+        self.assertEqual("J - Check Check - J", new_state)
+
+        info_state = "Q - Check"
+        board = "J"
+        action_history = [ "Check", "Check" ]
+
+        new_state = GC.add_action_history(info_state, board, action_history)
+
+        self.assertEqual("Q - Check Check - J - Check", new_state)
+
+        info_state = "JD"
+        board = "JC"
+        action_history = [ "Check", "Check" ]
+
+        new_state = GC.add_action_history(info_state, board, action_history)
+
+        self.assertEqual("JD - Check Check - JC", new_state)
+    
+    def test_postflop_game_can_have_adjusted_payoffs_and_info_states(self):
+        postflop_subgame = {
+            "player": 0,
+            "info_state": "J",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                { "utility": [ 0, 2 ] },
+                {
+                    "player": 1,
+                    "info_state": "Q - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        { "utility": [ 3, -1 ] }
+                    ]
+                }
+            ]
+        }
+
+        actual_subgame = GC.adjust_payoffs_and_actions(postflop_subgame, 4, "J", ["Check", "Check"])
+
+        expected_subgame = {
+            "player": 0,
+            "info_state": "J - Check Check - J",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                { "utility": [ -1, 3 ] },
+                {
+                    "player": 1,
+                    "info_state": "Q - Check Check - J - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        { "utility": [ 3, -1 ] },
+                        { "utility": [ 4, -2 ] }
+                    ]
+                }
+            ]
+        }
+
+        self.assertEqual(expected_subgame, actual_subgame)
+    
+    def test_append_a_post_flop_to_a_preflop_subgame(self):
+        preflop_subgame = {
+            "player": 0,
+            "info_state": "J",
+            "action_names": [ "Check", "Bet" ],
+            "next": [
+                {
+                    "player": 1,
+                    "info_state": "Q - Check",
+                    "action_names": [ "Check", "Bet" ],
+                    "next": [
+                        2,
+                        {
+                            "player": 0,
+                            "info_state": "J - Check Bet",
+                            "action_names": [ "Fold", "Call" ],
+                            "next": [
+                                { "utility": [ 0, 2 ] },
+                                4
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "player": 1,
+                    "info_state": "Q - Bet",
+                    "action_names": [ "Fold", "Call" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        4
+                    ]
+                }
+            ]
+        }
+
+        postflop_subgame = {
+            "player": 0,
+            "info_state": "J",
+            "action_names": [ "Fold", "Check" ],
+            "next": [
+                { "utility": [ 0, 2 ] },
+                {
+                    "player": 1,
+                    "info_state": "Q - Check",
+                    "action_names": [ "Fold", "Check" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        { "utility": [ -1, 3 ] }
+                    ]
+                }
+            ]
+        }
         
-    def test_full_game_generation(self):
+        actual_subgame = GC.append_postflop_subgame(preflop_subgame, postflop_subgame, "K")
+
+        expected_subgame = {
+            "player": 0,
+            "info_state": "J",
+            "action_names": [ "Check", "Bet" ],
+            "next": [
+                {
+                    "player": 1,
+                    "info_state": "Q - Check",
+                    "action_names": [ "Check", "Bet" ],
+                    "next": [
+                        {
+                            "player": 0,
+                            "info_state": "J - Check Check - K",
+                            "action_names": [ "Fold", "Check" ],
+                            "next": [
+                                { "utility": [ 0, 2 ] },
+                                {
+                                    "player": 1,
+                                    "info_state": "Q - Check Check - K - Check",
+                                    "action_names": [ "Fold", "Check" ],
+                                    "next": [
+                                        { "utility": [ 2, 0 ] },
+                                        { "utility": [ -1, 3 ] }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "player": 0,
+                            "info_state": "J - Check Bet",
+                            "action_names": [ "Fold", "Call" ],
+                            "next": [
+                                { "utility": [ 0, 2 ] },
+                                {
+                                    "player": 0,
+                                    "info_state": "J - Check Bet Call - K",
+                                    "action_names": [ "Fold", "Check" ],
+                                    "next": [
+                                        { "utility": [ -1, 3 ] },
+                                        {
+                                            "player": 1,
+                                            "info_state": "Q - Check Bet Call - K - Check",
+                                            "action_names": [ "Fold", "Check" ],
+                                            "next": [
+                                                { "utility": [ 3, -1 ] },
+                                                { "utility": [ -2, 4 ] }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "player": 1,
+                    "info_state": "Q - Bet",
+                    "action_names": [ "Fold", "Call" ],
+                    "next": [
+                        { "utility": [ 2, 0 ] },
+                        {
+                            "player": 0,
+                            "info_state": "J - Bet Call - K",
+                            "action_names": [ "Fold", "Check" ],
+                            "next": [
+                                { "utility": [ -1, 3 ] },
+                                {
+                                    "player": 1,
+                                    "info_state": "Q - Bet Call - K - Check",
+                                    "action_names": [ "Fold", "Check" ],
+                                    "next": [
+                                        { "utility": [ 3, -1 ] },
+                                        { "utility": [ -2, 4 ] }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+        self.assertEqual(expected_subgame, actual_subgame)
+        
+    def test_full_kuhn_game_generation(self):
         game_template = {
             "player": 0,
             "info_state": "%s",
